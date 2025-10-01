32c82ab64ec33e7cc89f1bdab7ed9e6e2e185816
Romain Fournier <rmn.fournier@gmail.com>
2025-10-01 14:39:28 -0400

    [feat] Bayes annotation

---
+++ src/snpbias/annotation/BayesAnnotator.py
@@ -0,0 +1,23 @@
+from snpbias.annotation.Chi2Annotator import Chi2Annotator
+from scipy.stats import chi2
+import numpy as np
+
+
+class BayesAnnotator(Chi2Annotator):
+    def __init__(self, df):
+        super().__init__(df)
+        self.pseudo_count = 1
+
+    def compute_score(self, row):
+        contingency_table = self.build_contingency_table(row, pseudocount=self.pseudo_count)
+        chi2_stat, p_value = self.chi2_test(contingency_table)
+        p_data_invert = self.p_data_invert(contingency_table)
+        p_data_under_h0 = chi2.pdf(chi2_stat, df=len(self.technologies) - 1)
+        if p_data_invert == 0:
+            return 0
+        return p_data_under_h0 * p_data_invert
+
+    def p_data_invert(self, contingency_table):
+        contingency_table = np.array(contingency_table)
+        p_data_invert = np.prod(np.sum(contingency_table, axis=1) - self.pseudo_count * contingency_table.shape[1])
+        return p_data_invert 
--- src/snpbias/entrypoints/annotate_snp.py
+++ src/snpbias/entrypoints/annotate_snp.py
@@ -1,6 +1,7 @@
 import argparse
 from snpbias.annotation.FreqAnnotator import FreqAnnotator
 from snpbias.annotation.Chi2Annotator import Chi2Annotator
+from snpbias.annotation.BayesAnnotator import BayesAnnotator
 import pandas as pd
 
 
@@ -8,7 +9,7 @@ def get_arguments():
     parser = argparse.ArgumentParser(description="Calculate SNP bias annotations from count data")
     parser.add_argument('--input', type=str, help='Input file with count data by technology', required=True)
     parser.add_argument('--out', type=str, help='Output file', required=True)
-    parser.add_argument('--method', type=str, choices=['freq', 'chi2'], required=True,
+    parser.add_argument('--method', type=str, choices=['freq', 'chi2', 'bayes'], required=True,
                         help='Annotation method to use')
     args = parser.parse_args()
     return args
@@ -20,6 +21,8 @@ def get_snp_annotator(args):
         annotator = FreqAnnotator(df)
     elif args.method == 'chi2':
         annotator = Chi2Annotator(df)
+    elif args.method == 'bayes':
+        annotator = BayesAnnotator(df)
     return annotator
 
 
